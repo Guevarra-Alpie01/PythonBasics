71b40a9372880a71e0dfda074dc9cc6e9fb7bf19
alpieguevarra <alpieguevarra12@gmail.com>
2026-03-12 22:41:45 +0800

    added learnings

--- day1.py
+++ day1.py
@@ -31,3 +31,24 @@ print(type(g))
 print(type(x))
 print(type(y))
 print(type(z))
+
+
+#Type casting a variable
+number = "10"
+
+cast = int(number) #convert a string into an integer
+print(cast)
+print(type(cast))
+
+
+#object reference variable
+ref = 120
+copy_ref = ref
+print(f"this is a copy:{copy_ref}")
+print(f"this is the original:{ref}")
+
+ref = "new ref"         #declare new value for ref so the declaration above wil not take effect
+print(f"{ref}") 
+copy_ref = ref
+print(copy_ref)         #copy the new ref
+    
