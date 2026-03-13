d2ea6a142751fa6b38fbb68521e21abb37fcb248
alpieguevarra <alpieguevarra12@gmail.com>
2026-03-14 01:33:02 +0800

    Christmas tree

--- forloop.py
+++ forloop.py
@@ -1,4 +1,4 @@
-
+# loop that stops till it reach 20
 course = 100      #  A list of courses
 
 for check in range(course):
@@ -8,5 +8,46 @@ for check in range(course):
     else:
         print("reach 20")
         break
-    
- 
+
+
+#loop through tuples
+tups = ("app", "hap", "dap" , "lap")    
+
+for index in range(len(tups)):
+    print(index)
+
+
+#nested loops 
+
+print("this is a nested loops ")
+
+
+
+for a in range(10):
+    for j in range( 2 * a + 1):
+        print("*",end='')
+    print()
+        
+print()
+print("christmas tree")
+
+
+#created a Christmastree
+christmastree = 20
+
+for j in range(christmastree):
+    for a in range(christmastree -j -1):
+        print(" " ,end='')
+
+    for i in range( 2 * j + 1):
+        print("*",end='')
+
+    print()
+
+     
+truck=5
+for truck in range(5):
+    for x in range(christmastree -1):
+        print(" ", end='')
+    print("|")
+print() 
