0b8bc59ed646ca4e54057ea03a1546971a70af2e
alpieguevarra <alpieguevarra12@gmail.com>
2026-03-14 00:29:04 +0800

    for loop

--- day2.py
+++ day2.py
@@ -48,3 +48,57 @@ new = Color("pink", "rainbow")              #create an object of the class and p
 
 print(f"first color: {new.color1}")         #print the value of the instance variable color1 using the object new
 print(f"second color: {new.color2}")        #print the value of the instance variable color2 using the object new
+
+
+
+#create another sample of  class 
+
+
+class House:
+    def __init__(self, door, window):
+        self.door = door
+        self.window = window
+
+blueprint = House("wooden door", "glass window")
+builder = House("panday" , "engineer")
+
+print(blueprint.door)
+print(blueprint.window)
+print(f"builder of door {builder.door}")
+print(f" builder of window:{builder.window}")
+
+
+                                    #Conditional statament 
+                                    #nested if statement 
+
+breed1 = 40
+breed2 = 25
+
+if breed1 >=35:                     #check if the breed1 is greater or equal to 35 return true if that condition is met
+    print("breed is sufficient!")
+    #run if the condition above is met
+    if breed2 >30:    #execute if breed 2 is above 30
+        print("breed 2 is higher!!")
+
+    elif breed2 ==30:   #execute if breed2 is equal to 30
+        print("breed 2 is equal to 30")
+
+    else:   #if no condition is met then execute this 
+        print ("breed 2 is lower to 30")   
+else:                               #execute if breed 1 is less than 35
+    print("invalid breed!!")
+
+
+"""Check Even or Odd
+Input: A number from the user
+Output: "Even" or "Odd"
+Concepts: if, else, % operator
+"""
+
+input = int(input("Enter one number:"))
+
+if input % 2:
+    print ("odd number")
+
+else:
+    print("even number")
---
+++ forloop.py
@@ -0,0 +1,12 @@
+
+course = 100      #  A list of courses
+
+for check in range(course):
+    if check <= 20:
+        print(check)
+        continue
+    else:
+        print("reach 20")
+        break
+    
+ 
